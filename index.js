1b0c75ecb3fbf8f86c74df4eff82d0e3bf6666d2
emmanuellerayssa <emmanuelleyankam@yahoo.fr>
2023-09-20 10:19:17 +0100

    create function with arg

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function name() {
-    var jsNode = document.querySelector("#is-js-loaded");
-    jsNode.innerText = "Js is charged";
-    jsNode.style.color = "yellow";
-    jsNode.style.textAlign = "center";
-     
+function initJs(color) {
+  console.log("Show the selected color: ");
+  console.log(color);
+  var jsNode = document.querySelector("#is-js-loaded");
+  jsNode.innerText = "Js is charged";
+  jsNode.style.color = color;
+  jsNode.style.textAlign = "center";
 }
 
-initJs();
-
+initJs("DarkMagenta");

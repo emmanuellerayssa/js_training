27eebcc5e68dd5d0cdd000c92f36922e05c5d49e
emmanuellerayssa <emmanuelleyankam@yahoo.fr>
2023-09-20 10:23:14 +0100

    display in console + time computing

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function initJs(color) {
-  console.log("Show the selected color: ");
-  console.log(color);
+  console.time("timekeeper");
+  console.log("Show the arguments: ");
+  console.log(arguments);
+  console.trace(arguments);
+  console.warn(arguments);
+  console.error(arguments);
   var jsNode = document.querySelector("#is-js-loaded");
   jsNode.innerText = "Js is charged";
   jsNode.style.color = color;
   jsNode.style.textAlign = "center";
+  console.timeEnd("timekeeper");
 }
 
-initJs("DarkMagenta");
+initJs("DarkMagenta", { a: 34, b: "123654" });

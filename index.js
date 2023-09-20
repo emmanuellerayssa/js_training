f10444af618a4324155d1431c64c72f14101d768
emmanuellerayssa <emmanuelleyankam@yahoo.fr>
2023-09-20 10:07:34 +0100

    function declaration + utilisation

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var jsNode = document.querySelector("#is-js-loaded");
-jsNode.innerText = "Js is charged"
-jsNode.style.color='yellow'
-jsNode.style.textAlign='center'
+function name() {
+    var jsNode = document.querySelector("#is-js-loaded");
+    jsNode.innerText = "Js is charged";
+    jsNode.style.color = "yellow";
+    jsNode.style.textAlign = "center";
+     
+}
+
+initJs();
+

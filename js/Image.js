efd019783f5fd05832fb864e4e07292c832bf0d1
emmanuellerayssa <emmanuelleyankam@yahoo.fr>
2023-09-21 10:24:15 +0100

    getter+setter

--- js/Image.js
+++ js/Image.js
@@ -1,23 +1,31 @@
 class ImageShort {
   #uid; // # means private
-    w = 100; // No need to use const or var in ES6
+  w = 100; // No need to use const or var in ES6
   h;
   url;
 
-  constructor(img) {
-    this.#uid=Math.random
-    if (undefined === img) img = {};
-    if (undefined !== img.w) {
-      this.w = img.w;
-    }
-    this.h = undefined !== img.h ? img.h : 100; //Operateur ternaire. Moins rapide qu'un if
-    if (undefined !== img.url && img.url.length >= 5) {
-      this.url = img.url;
-    }
-    //else if(){}
-    else {
-      this.url = "";
-    }
+  /**
+   *
+   * @param {*Object}
+   */
+
+  constructor(img = {}) {}
+
+  /**
+   * Getter de #uid
+   */
+  get uid() {
+    return this.#uid;
+  }
+
+  /**
+   * Setter de #uid
+   */
+  set uid(value) {
+    this.#uid = value;
+  }
+  getRatioWH() {
+    return this.w / this.h;
   }
 }
 
@@ -25,12 +33,13 @@ class ImageMeme extends ImageShort {
   //heritage
   title = "no image";
   id = undefined;
-  constructor(img) {
+  constructor(img = {}) {
+    //if (unefined=== img) img = {};
     super(img); //We call parent constructor from the child constructor
     if (undefined !== img.title && img.title.length > 2) {
       this.title = img.title;
     } else if (
-      undefined === img.title && 
+      undefined === img.title &&
       undefined !== img.url &&
       img.url.length > 5
     ) {
